581a49384f621a452f050ddc64082ec18bd1a0f9
Hayden <66452827+HaydenCJuneau@users.noreply.github.com>
2025-03-25 19:18:21 -0400

    Finished Task 2

--- src/task2_engagement_by_age.py
+++ src/task2_engagement_by_age.py
@@ -1,14 +1,22 @@
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.functions import avg, col
 
 spark = SparkSession.builder.appName("EngagementByAgeGroup").getOrCreate()
 
 # Load datasets
-posts_df = spark.read.option("header", True).csv("input/posts.csv", inferSchema=True)
-users_df = spark.read.option("header", True).csv("input/users.csv", inferSchema=True)
+posts_df = spark.read.option("header", True).csv("/opt/bitnami/spark/Social/input/input/posts.csv", inferSchema=True)
+users_df = spark.read.option("header", True).csv("/opt/bitnami/spark/Social/input/input/users.csv", inferSchema=True)
 
-# TODO: Implement the task here
+def engagement_by_age(posts: DataFrame, users: DataFrame) -> DataFrame:
+    df = posts.join(users, posts.UserID == users.UserID, "inner")
 
+    
+    averages = df.groupBy("AgeGroup").agg(
+        avg(df.Likes).alias("avg_likes"), 
+        avg(df.Retweets).alias("avg_retweets"),
+    )
+
+    return averages.orderBy("avg_likes", ascending=False)
 
 # Save result
-engagement_df.coalesce(1).write.mode("overwrite").csv("outputs/engagement_by_age.csv", header=True)
+engagement_by_age(posts_df, users_df).coalesce(1).write.mode("overwrite").csv("outputs/engagement_by_age.csv", header=True)
